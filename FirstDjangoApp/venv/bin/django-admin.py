70bb55cd897eabc0a5ec56aafd07034000abefb8
joyfull <751081818@qq.com>
2019-11-22 16:54:07 +0900

    django first app

---
+++ FirstDjangoApp/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/marlonleng/LearnSpace/PythonWeb/FirstDjangoApp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- PyShop/products/views.py
+++ PyShop/products/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from  .models import Product
+from .models import Product
+import json
+from django.core import serializers
+
 
 def index(request):
-    products = Product.objects.all()
 
-    return render(request, 'index.html', {'products':products})
-    # return HttpResponse('Hello World')
+    products = Product.objects.all()
+    jsonData = serializers.serialize('json', products)
+    return HttpResponse(jsonData)
+
+    jsonlist = []
+    for p in products:
+        jsondict = {
+            "name": p.name,
+            "price": p.price,
+            "stock": p.stock,
+            "image_url": p.image_url
+        }
+        jsonlist.append(jsondict)
+
+    # data = "[" + str(jsonlist) + "]"
+
+    return HttpResponse(str(jsonlist))
+
+    if request.headers['api_type'] == 'app':
+        return HttpResponse(json.dump(products))
+    else:
+        return render(request, 'index.html', {'products': products})
 
 
 def product_new(request):
