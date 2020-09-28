a4bb66638670dbfc8b92c21d55bc38c8376af47e
joyfull <751081818@qq.com>
2020-09-28 17:24:54 +0900

    wip

--- trydjango/venv/src/trydjango/blogs/views.py
+++ trydjango/venv/src/trydjango/blogs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
+
+# 新增文章
+# 删除文章
+# 修改文章
+# 展示文章列表
 
 def blogs_list(request):
     return HttpResponse("<h1> blogs list </h1>")
--- trydjango/venv/src/trydjango/products/models.py
+++ trydjango/venv/src/trydjango/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    
     title   = models.CharField(max_length=120) 
     desc    = models.TextField(blank=True,null=True)
     price   = models.DecimalField(decimal_places=2, max_digits=1000)
